5f0f839fcc3332e4687c7f00f0dcadb7574a54bc
Marcin <60314265+marcinjedrzejowski@users.noreply.github.com>
2020-12-17 18:25:25 +0100

    Added dynamic grid with current weather (#17)
    * Logged user page + css
    
    * Added grid to main page
    
    * Centering the map on searched city
    
    * Added weather boxes and logged user page
    
    * Added dynamic grid

--- front/js/main.js
+++ front/js/main.js
@@ -2,11 +2,15 @@ import { WeatherApi } from './WeatherApi.js';
 import { LeafletMap } from './LeafletMap.js';
 import { DateTime } from './DateTime.js';
 import { WeatherChart } from './WeatherChart.js';
+import { WeatherGrid } from './WeatherGrid.js';
 
 const api = new WeatherApi("7ded80d91f2b280ec979100cc8bbba94");
 const map = new LeafletMap(api, "e97c7311758c0dc6edec263d72155863");
 const dt = new DateTime();
 const chart = new WeatherChart(map, api, dt);
+const grid = new WeatherGrid(api);
+
+["London", "Madrid", "Paris", "Berlin"].forEach(async (cityName, index) => await grid.create(cityName, index))
 
 chart.create();
 
@@ -22,55 +26,55 @@ removeMarkersButton.addEventListener("click", map.removeAllMarkers);
 const getWeatherForSearchedCity = async () => {
     let cityName = searchInput.value;
 
-        if (cityName.length >= 3) {
-            let data = await api.getWeatherByName(cityName);
+    if (cityName.length >= 3) {
+        let data = await api.getWeatherByName(cityName);
 
+        let currentWeather = { 
+            cityName: cityName, 
+            icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
+            name: data.weather[0].main,
+            temperature: {
+                current: data.main.temp,
+                min: data.main.temp_min,
+                max: data.main.temp_max,
+                feelsLike: data.main.feels_like
+            },
+            humidity: data.main.humidity,
+            pressure: data.main.pressure,
+            latitude: data.coord.lat,
+            longitude: data.coord.lon
+        };
 
-            let currentWeather = { 
-                cityName: cityName, 
-                icon: `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`,
-                name: data.weather[0].main,
-                temperature: {
-                    current: data.main.temp,
-                    min: data.main.temp_min,
-                    max: data.main.temp_max,
-                    feelsLike: data.main.feels_like
-                },
-                humidity: data.main.humidity,
-                pressure: data.main.pressure,
-                latitude: data.coord.lat,
-                longitude: data.coord.lon
-            };
+        let currentWeatherElement = document.createElement("div");
 
-            let currentWeatherElement = document.createElement("div");
+        currentWeatherElement.innerHTML = `
+                <img src=${currentWeather.icon} style="width: 100px; height: 100px;" alt=${currentWeather.name}>
+                <div>
+                    <h5>${currentWeather.cityName}</h5> 
+                    <p>
+                        <div>
+                            <p><strong>${currentWeather.temperature.current}°C</strong>
+                            but feels like: ${currentWeather.temperature.feelsLike}°C</p>
+                        </div>
+                    </p>
+                    <p><small>Humidity: ${currentWeather.humidity}% Pressure: ${currentWeather.pressure}hPA</small></p>
+                    <p><small>Wind: 13 km/h | Air Quality: Moderate</small></p>
+                </div>
+        `
 
-            currentWeatherElement.innerHTML = `
-                    <img src=${currentWeather.icon} style="width: 100px; height: 100px;" alt=${currentWeather.name}>
-                    <div>
-                        <h5>${currentWeather.cityName}</h5> 
-                        <p>
-                            <div>
-                                <p><strong>${currentWeather.temperature.current}°C</strong>
-                                but feels like: ${currentWeather.temperature.feelsLike}°C</p>
-                            </div>
-                        </p>
-                        <p><small>Humidity: ${currentWeather.humidity}% Pressure: ${currentWeather.pressure}hPA</small></p>
-                    </div>
-            `
+        searchedWeather.hidden = false;
+        searchedWeather.innerHTML = '';
+        searchedWeather.appendChild(currentWeatherElement);
 
-            searchedWeather.hidden = false;
-            searchedWeather.innerHTML = '';
-            searchedWeather.appendChild(currentWeatherElement);
+        await map.refreshCoords({ 'coords': {
+            'latitude': currentWeather.latitude,
+            'longitude': currentWeather.longitude
+        }})
 
-            await map.refreshCoords({ 'coords': {
-                'latitude': currentWeather.latitude,
-                'longitude': currentWeather.longitude
-            }})
+        await map.refreshMap()
 
-            await map.refreshMap()
-
-            chart.create();
-        }
+        chart.create();
+    }
 }
 
 searchInput.addEventListener('keyup', ({key}) => {
